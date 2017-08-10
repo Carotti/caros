9d551d144eb815c4e7eda95d0ac146ca60da2301
Thomas Carotti <thomas@carotti.co.uk>
2017-08-10 15:08:07 +0100

    Back to a known point, on with GDT, IDT etc

--- kernel/kernel/idt.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <kernel/idt.h>
-
-__attribute__((interrupt)) void interrupt_handler(struct interrupt_frame* frame)
-{
-    
-}
--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -9,13 +9,5 @@ extern void init_arch();
 void kernel_main(void)
 {
 	terminal_initialize();
-	init_arch();
 	printf("Welcome to Caros!");
-}
-
-// CPU had a panic, give some sort of error message
-void kernel_cpu_panic()
-{
-	register int i asm("eax");
-	printf("Kernel panic! Err: %d", i);
 }
